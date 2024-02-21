0c3467a9fed29fe989b03458d8c56887b8cc6e8a
Santoro Thomas <thomas_santoro@ens.univ-artois.fr>
2024-02-21 08:53:59 +0100

    add enumerations

---
+++ src/main/java/fr/univartois/butinfo/s5a01/musicmatcher/utils/Region.java
@@ -0,0 +1,16 @@
+package fr.univartois.butinfo.s5a01.musicmatcher.utils;
+
+/**
+ * This is an enumeration of some regions of the worlds
+ */
+public enum Region {
+	
+	SOUTH_AMERICA,
+	NORTH_AMERICA,
+	EUROPE,
+	ASIA,
+	OCEANIA,
+	AFRICA,
+	ANTARCTICA;
+	
+}
