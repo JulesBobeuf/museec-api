b9e86692281cb466521778e9b70cd075dbf62a97
Santoro Thomas <thomas_santoro@ens.univ-artois.fr>
2024-03-11 15:56:46 +0100

    add methods and tests to accept and reject musician close#13

--- src/main/java/fr/univartois/butinfo/s5a01/musicmatcher/document/ApiUser.java
+++ src/main/java/fr/univartois/butinfo/s5a01/musicmatcher/document/ApiUser.java
@@ -78,7 +78,7 @@ public class ApiUser implements UserDetails {
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		return role.getAuthorities();
 	}
-
+	
 	@Override
 	public String getPassword() {
 		return password;
--- src/main/java/fr/univartois/butinfo/s5a01/musicmatcher/service/OfferService.java
+++ src/main/java/fr/univartois/butinfo/s5a01/musicmatcher/service/OfferService.java
@@ -5,8 +5,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import fr.univartois.butinfo.s5a01.musicmatcher.auth.Role;
 import fr.univartois.butinfo.s5a01.musicmatcher.document.ApiUser;
+import fr.univartois.butinfo.s5a01.musicmatcher.document.Band;
 import fr.univartois.butinfo.s5a01.musicmatcher.document.Offer;
+import fr.univartois.butinfo.s5a01.musicmatcher.repository.BandRepository;
 import fr.univartois.butinfo.s5a01.musicmatcher.repository.OfferRepository;
 import fr.univartois.butinfo.s5a01.musicmatcher.repository.UserRepository;
 
@@ -18,11 +21,14 @@ public class OfferService {
 	
 	@Autowired
 	private OfferRepository offerRepository;
+	
+	@Autowired
+	private BandRepository bandRepository;
 
     /**
      * Method that allows a user to accept an offer
      */
-    public Boolean accepteOffer(int idUser, int idOffer) {
+    public boolean accepteOffer(int idUser, int idOffer) {
     	Optional<ApiUser> user = userRepository.findById(idUser);
     	Optional<Offer> offer = offerRepository.findById(idOffer);
     	if (user.isEmpty() || offer.isEmpty()) {
@@ -40,7 +46,7 @@ public class OfferService {
     /**
      * Method that allows a user to reject an offer
      */
-    public Boolean rejectOffer(int idUser, int idOffer) {
+    public boolean rejectOffer(int idUser, int idOffer) {
     	Optional<ApiUser> user = userRepository.findById(idUser);
     	Optional<Offer> offer = offerRepository.findById(idOffer);
     	if (user.isEmpty() || offer.isEmpty()) {
@@ -53,5 +59,70 @@ public class OfferService {
     	return true;
     }
   
+    /**
+     * Method that allows an owner to accept a musician
+     */
+	public boolean acceptMusician(String email, int idOffer, int idMusician) {
+		Optional<ApiUser> musician = userRepository.findById(idMusician);
+    	Optional<Offer> offer = offerRepository.findById(idOffer);
+		Optional<ApiUser> owner = userRepository.findByEmail(email);
+		
+    	if (musician.isEmpty() || offer.isEmpty() || owner.isEmpty()) {
+    		return false;
+    	}
+    	
+    	ApiUser realMusician = musician.get();
+    	ApiUser realOwner = owner.get();
+    	Offer realOffer = offer.get();
+    	Optional<Band> band = bandRepository.findById(realOwner.getIdBand());
+    	Band realBand = band.get();
+    	
+    	if (realBand.getOwner() != realOwner.getId()) {
+			if (realOwner.getRole() != Role.ADMINISTRATOR) {
+	            throw new IllegalArgumentException("Forbidden");
+			}
+		}
+    	
+    	if (realMusician.isLookingForAGroup() && realMusician.getIdBand() == -1 && realOffer.getAwaitingMembers().contains(realMusician.getId())) {
+    		realMusician.setIdBand(realOwner.getIdBand());
+    		realMusician.setLookingForAGroup(false);
+    		realOffer.setActive(false);
+    		userRepository.save(realMusician);
+    		offerRepository.save(realOffer);
+    		return true;
+    	}
+    	return false;
+	}
 	
+    /**
+     * Method that allows an owner to accept a musician
+     */
+	public boolean rejectMusician(String email, int idOffer, int idMusician) {
+		Optional<ApiUser> musician = userRepository.findById(idMusician);
+    	Optional<Offer> offer = offerRepository.findById(idOffer);
+		Optional<ApiUser> owner = userRepository.findByEmail(email);
+		
+    	if (musician.isEmpty() || offer.isEmpty() || owner.isEmpty()) {
+    		return false;
+    	}
+    	
+    	ApiUser realMusician = musician.get();
+    	ApiUser realOwner = owner.get();
+    	Offer realOffer = offer.get();
+    	Optional<Band> band = bandRepository.findById(realOwner.getIdBand());
+    	Band realBand = band.get();
+    	
+    	if (realBand.getOwner() != realOwner.getId()) {
+			if (realOwner.getRole() != Role.ADMINISTRATOR) {
+	            throw new IllegalArgumentException("Forbidden");
+			}
+		}
+    	
+    	if (realOffer.getAwaitingMembers().contains(realMusician.getId())) {
+    		realOffer.addUsersThatRejected(realMusician.getId());
+    		offerRepository.save(realOffer);
+    		return true;
+    	}
+    	return false;
+	}
 }
