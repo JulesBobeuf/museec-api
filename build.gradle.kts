52f70c1fd2def58c93dc806c3b785cf8707395cf
Bobeuf Jules <jules_bobeuf@ens.univ-artois.fr>
2024-03-13 09:29:41 +0100

    [U] fix ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,13 @@ java {
 }
 
 repositories {
-    gradlePluginPortal()
-    google()
     mavenCentral()
+    maven {
+        url = uri("https://repo.spring.io/release")
+    }
+    maven {
+        url = uri("https://repository.jboss.org/maven2")
+    }
 }
 
 dependencies {
