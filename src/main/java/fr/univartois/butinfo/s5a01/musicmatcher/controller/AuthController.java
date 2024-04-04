0f78d6999aa2c0b35ccb1fb758724b8d2f7aca4d
Bobeuf Jules <jules_bobeuf@ens.univ-artois.fr>
2024-04-04 09:02:48 +0200

    Allow CORS

--- src/main/java/fr/univartois/butinfo/s5a01/musicmatcher/controller/AuthController.java
+++ src/main/java/fr/univartois/butinfo/s5a01/musicmatcher/controller/AuthController.java
@@ -2,6 +2,7 @@ package fr.univartois.butinfo.s5a01.musicmatcher.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import jakarta.validation.Valid;
 
 @Tag(name = "Auth", description = "Authentication endpoint")
 @RestController
+@CrossOrigin
 @RequestMapping("/api/auth")
 public class AuthController {
 
--- src/main/java/fr/univartois/butinfo/s5a01/musicmatcher/controller/RecommendationController.java
+++ src/main/java/fr/univartois/butinfo/s5a01/musicmatcher/controller/RecommendationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Recommendation", description = "Recommendation endpoint")
 @RestController
+@CrossOrigin
 @RequestMapping("/api/recommendation")
 public class RecommendationController {
 
